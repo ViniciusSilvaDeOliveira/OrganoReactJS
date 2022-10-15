3a0aa3a18491e814d3fbeef90f223b24a2ac6540
Vinicius Silva de Oliveira <69611880+ViniciusSilvaDeOliveira@users.noreply.github.com>
2022-10-15 11:29:56 -0300

    descrição do commit

--- src/componentes/CampoTexto/index.js
+++ src/componentes/CampoTexto/index.js
@@ -4,12 +4,16 @@ const CampoTexto = (props) => {
 
     const placeholderModificaado = `${props.placeholder}...`
 
+    const aoDigitado = (evento) => {
+        props.aoAlterado(evento.target.value)
+    }
+
     return (
         <div className="campo-texto">
             <label>
                 {props.label}
             </label>
-            <input required={props.obrigatorio} placeholder={placeholderModificaado} />
+            <input value={props.valor} onChange={aoDigitado} required={props.obrigatorio} placeholder={placeholderModificaado} />
         </div>
     )
 }
---
+++ src/componentes/Time/index.js
@@ -0,0 +1,15 @@
+import './Time.css'
+
+const Time = (props) => {
+    const css = {backgroundColor: props.corSecundaria}
+
+    const borda = {borderColor: props.corPrimaria}
+
+    return (
+        <section className='time' style={css}>
+            <h3 style={borda}>{props.nome}</h3>
+        </section>
+    )
+}
+
+export default Time
