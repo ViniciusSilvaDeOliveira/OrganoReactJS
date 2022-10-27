5bcf86798a0cb6607e714a3286cd6bbaf0b49a99
Vinicius Silva de Oliveira <69611880+ViniciusSilvaDeOliveira@users.noreply.github.com>
2022-10-27 19:34:46 -0300

    Projeto finalizado

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -1,3 +1,4 @@
+import Colaborador from '../Colaborador'
 import './Time.css'
 
 const Time = (props) => {
@@ -6,9 +7,13 @@ const Time = (props) => {
     const borda = {borderColor: props.corPrimaria}
 
     return (
-        <section className='time' style={css}>
+        (props.colaboradores.length > 0) ? <section className='time' style={css}>
             <h3 style={borda}>{props.nome}</h3>
+            <div className='colaboradores'>
+                {props.colaboradores.map(colaborador => <Colaborador corDeFundo={props.corPrimaria} key={colaborador.nome} nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem}/>)}
+            </div>            
         </section>
+        : ''
     )
 }
 
